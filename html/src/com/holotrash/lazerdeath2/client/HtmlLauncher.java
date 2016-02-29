f4c80f359d47eccf1890b1725376587de1d1bc21
holotrash <gmurderer@nightdick.com>
2016-02-29 02:17:20 -0600

    first commit

---
+++ html/src/com/holotrash/lazerdeath2/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.holotrash.lazerdeath2.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.holotrash.lazerdeath2.lazerdeath2;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new lazerdeath2();
+        }
+}
